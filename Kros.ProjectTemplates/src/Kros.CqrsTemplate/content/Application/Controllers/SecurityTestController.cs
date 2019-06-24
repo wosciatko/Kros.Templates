59266680a35c201c1bf3d9399c9b2b7ba9dc8cb1
Vojtěch Rojíček <rojicek@kros.sk>
2019-06-24 16:43:17 +0200

    Added security controller, swagger produces update

---
+++ Kros.ProjectTemplates/src/Kros.CqrsTemplate/content/Application/Controllers/SecurityTestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Kros.AspNetCore.Authorization;
+
+namespace Kros.CqrsTemplate.Application.Controllers
+{
+    /// <summary>
+    /// SecurityTest controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ValidationProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+    public class SecurityTestController : ControllerBase
+    {
+        /// <summary>
+        /// Authorized endpoint.
+        /// </summary>
+        [HttpGet("authorized")]
+        public ActionResult<string> Authorized() => Ok(new { Email = User.GetUserEmail() });
+
+        /// <summary>
+        /// AllowAnonymous endpoint.
+        /// </summary>
+        [HttpGet("unauthorized")]
+        [AllowAnonymous]
+        public ActionResult UnauthorizedEndpoint() => Ok();
+    }
+}
